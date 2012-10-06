d3f0494c95af657891704388009b0572d4d13d69
James Fargher <proglottis@gmail.com>
2012-10-07 00:26:39 +1300

    Helpers to find current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :user_logged_in?
+  def user_logged_in?
+    current_user != nil
+  end
+
 end
