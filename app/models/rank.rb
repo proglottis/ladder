877fe8935dc5dbc6bda6adc771e4cacf550c0c62
James Fargher <proglottis@gmail.com>
2012-10-07 22:19:12 +1300

    Calculate trueskill ranking

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -3,4 +3,8 @@ class Rank < ActiveRecord::Base
   belongs_to :tournament
 
   validates_presence_of :user_id, :tournament_id
+
+  def rank
+    mu - 3.0 * sigma
+  end
 end
--- test/models/rank_test.rb
+++ test/models/rank_test.rb
@@ -1,7 +1,19 @@
 require "minitest_helper"
 
-class RankTest < MiniTest::Rails::ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Rank do
+  describe "#rank" do
+    let(:rank) { FactoryGirl.create(:rank) }
+
+    it "must be zero with default values" do
+      rank.mu = 25.0
+      rank.sigma = 25.0 / 3.0
+      rank.rank.must_equal 0.0
+    end
+
+    it "must be about 50 for maximum values" do
+      rank.mu = 56.995
+      rank.sigma = 1.901
+      rank.rank.must_be_close_to 51.292
+    end
+  end
 end
