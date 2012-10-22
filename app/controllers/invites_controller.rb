ff8f80e6fe5b648f020ac80378ef55f78823aaa9
James Fargher <proglottis@gmail.com>
2012-10-22 13:52:41 +1300

    Dry up invites controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,16 +1,13 @@
 class InvitesController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_invite_for_tournament, :only => [:show, :update]
+  before_filter :find_tournament_with_current_user, :only => [:new, :create]
 
   def show
-    @tournament = Tournament.find(params[:tournament_id])
-    @invite = @tournament.invites.available.find_by_code!(params[:id])
   end
 
   def update
-    @tournament = Tournament.find(params[:tournament_id])
-    @invite = @tournament.invites.available.find_by_code!(params[:id])
-    @invite.user = current_user
-    if @invite.save
+    if @invite.update_attributes(:user => current_user)
       redirect_to tournament_path(@tournament)
     else
       render :show
@@ -18,12 +15,10 @@ class InvitesController < ApplicationController
   end
 
   def new
-    @tournament = Tournament.participant(current_user).find(params[:tournament_id])
     @invite = @tournament.invites.build
   end
 
   def create
-    @tournament = Tournament.participant(current_user).find(params[:tournament_id])
     @invite = @tournament.invites.build(params.require(:invite).permit(:email))
     if @invite.save
       InviteMailer.invite_email(@invite).deliver
@@ -31,5 +26,16 @@ class InvitesController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  def find_invite_for_tournament
+    @tournament = Tournament.find(params[:tournament_id])
+    @invite = @tournament.invites.available.find_by_code!(params[:id])
+  end
+
+  def find_tournament_with_current_user
+    @tournament = Tournament.participant(current_user).find(params[:tournament_id])
   end
 end
