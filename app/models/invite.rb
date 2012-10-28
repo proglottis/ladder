fbbe26674ba0fea9069035a486e409f820b0c4b6
James Fargher <proglottis@gmail.com>
2012-10-28 15:18:17 +1300

    Restrict number of invites allowed

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,18 +1,30 @@
 require 'securerandom'
 
 class Invite < ActiveRecord::Base
+  OWNER_LIMIT = 10
+
   belongs_to :owner, :class_name => 'User'
   belongs_to :user
   belongs_to :tournament
 
   validates_presence_of :tournament_id, :code, :email
+  validate :maximum_allowed_by_owner
 
   before_validation :generate_code
   before_validation :generate_expires_at
 
+  def self.not_expired
+    invites = arel_table
+    where(invites[:expires_at].gt(Time.now))
+  end
+
   def self.available
     invites = arel_table
-    where(invites[:expires_at].gt(Time.now).and(invites[:user_id].eq(nil)))
+    not_expired.where(invites[:user_id].eq(nil))
+  end
+
+  def self.limit_left
+    OWNER_LIMIT - not_expired.count
   end
 
   def to_param
@@ -27,5 +39,11 @@ class Invite < ActiveRecord::Base
 
   def generate_expires_at
     self.expires_at = 7.days.from_now
+  end
+
+  def maximum_allowed_by_owner
+    if tournament.invites.where(:owner_id => owner).reload.limit_left < 1
+      errors.add(:base, 'Exceeded limit')
+    end
   end
 end
