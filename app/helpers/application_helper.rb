03c09e47d0d28db72f4a7532c033967a878e2ebb
James Fargher <proglottis@gmail.com>
2012-10-30 23:27:05 +1300

    Less annoying gravatar defaults, less typos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def gravitar_image_tag(email, size = 16)
+  def gravatar_image_tag(email, size = 16)
     hash = Digest::MD5.hexdigest(email.strip.downcase)
-    image_tag "http://www.gravatar.com/avatar/#{hash}?size=#{size}", :size => "#{size}x#{size}"
+    image_tag "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon", :size => "#{size}x#{size}"
   end
 
 end
