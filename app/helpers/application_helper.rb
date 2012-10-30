f17e6cca4793bfda0f43973b47a3ea18aa61f78c
James Fargher <proglottis@gmail.com>
2012-10-30 23:09:11 +1300

    Gravitar user icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gravitar_image_tag(email, size = 16)
+    hash = Digest::MD5.hexdigest(email.strip.downcase)
+    image_tag "http://www.gravatar.com/avatar/#{hash}?size=#{size}", :size => "#{size}x#{size}"
+  end
+
 end
