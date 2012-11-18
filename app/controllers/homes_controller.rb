d75b677e85ca54ce826a087260025995860199e7
James Fargher <proglottis@gmail.com>
2012-11-18 19:32:08 +1300

    Tidy up pagination

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def show
     if user_logged_in?
-      @games = Game.with_participant(current_user).order('games.updated_at DESC').page(params[:page]).per(5)
+      @games = Game.with_participant(current_user).order('games.updated_at DESC').page(params[:page]).per(10)
     end
   end
 end
