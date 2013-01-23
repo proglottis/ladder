43afa9779647542cd8ca7815cfc9359863777959
James Fargher <proglottis@gmail.com>
2013-01-23 22:23:22 +1300

    Configure minitest generators

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,9 @@ module Ladder
     config.action_mailer.default_url_options = { :host => 'ladder.nothing.co.nz' }
 
     config.exceptions_app = self.routes
+
+    config.generators do |g|
+      g.test_framework :mini_test, :spec => true, :fixture => false
+    end
   end
 end
