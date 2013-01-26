cd55d6c4970a98003571c9205182dc5e121ee980
James Fargher <proglottis@gmail.com>
2013-01-26 20:04:09 +1300

    Use actual secure gravatar URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def gravatar_image_tag(email, size = 16)
     hash = Digest::MD5.hexdigest(email.strip.downcase)
-    image_tag "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon", :size => "#{size}x#{size}"
+    image_tag "https://secure.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon", :size => "#{size}x#{size}"
   end
 
 end
