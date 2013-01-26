55fa6b32b47b6fdceccdf418182f27d1146e3e74
James Fargher <proglottis@gmail.com>
2013-01-26 20:36:33 +1300

    Show games on profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+    @games = Kaminari.paginate_array((Game.with_participant(current_user) & Game.with_participant(@user)).sort_by{|u| Time.zone.now - u.updated_at}).page(params[:page]).per(10)
   end
 end
