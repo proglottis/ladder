592a65c6c913da6459c021267f26d26f0dc01cf8
James Fargher <proglottis@gmail.com>
2013-01-29 18:15:17 +1300

    Remove old relations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ActiveRecord::Base
   has_many :services
   has_many :tournaments, :inverse_of => :owner, :foreign_key => :owner_id
   has_many :invites, :inverse_of => :owner, :foreign_key => :owner_id
-  has_many :ranks
-  has_many :elo_ratings
 end
