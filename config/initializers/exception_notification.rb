3296022b0bb64707723e552ab83dce4e183da7be
James Fargher <proglottis@gmail.com>
2013-07-02 16:32:26 +1200

    Fix exception notification

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   Rails.application.config.middleware.use ExceptionNotifier,
     :sender_address => 'ladders@ladders.pw',
-    :exception_recipients => ENV['LADDER_SERVICE_EMAIL'],
-    :ignore_exceptions => ExceptionNotifier.default_ignore_exceptions
+    :exception_recipients => ENV['LADDER_SERVICE_EMAIL']
 end
