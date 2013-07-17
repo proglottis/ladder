a860958f8105940978f9d6ff9dac398ebdd53fab
James Fargher <proglottis@gmail.com>
2013-07-17 19:29:56 +1200

    Fix migrations that break without the old migrations

--- db/migrate/20130706043430_backpopulate_players.rb
+++ db/migrate/20130706043430_backpopulate_players.rb
@@ -1,8 +1,8 @@
 class BackpopulatePlayers < ActiveRecord::Migration
   def up
     Tournament.find_each do |tournament|
-      tournament.users.uniq.each do |user|
-        tournament.players.create! :user => user
+      tournament.current_rating_period.ratings.each do |rating|
+        tournament.players.create! :user_id => rating.user_id
       end
     end
   end
--- db/migrate/20130713054448_backpopulate_player_id_on_ratings.rb
+++ db/migrate/20130713054448_backpopulate_player_id_on_ratings.rb
@@ -1,9 +1,11 @@
 class BackpopulatePlayerIdOnRatings < ActiveRecord::Migration
   def up
     Tournament.find_each do |tournament|
-      tournament.ratings.find_each do |rating|
-        player = tournament.players.find_by! user_id: rating.user_id
-        rating.update_attributes! player_id: player.id
+      tournament.rating_periods.each do |rating_period|
+        rating_period.ratings.find_each do |rating|
+          player = tournament.players.find_by! user_id: rating.user_id
+          rating.update_attributes! player_id: player.id
+        end
       end
     end
   end
