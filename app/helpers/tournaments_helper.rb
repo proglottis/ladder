e5b1d5584c5788f46d7d7c8f542e325820f241a0
James Fargher <proglottis@gmail.com>
2013-09-05 15:00:06 +1200

    Fix tournament and profile views

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -2,8 +2,8 @@ module TournamentsHelper
   def streak_badge(player)
     return unless player.streak?
 
-    colors = {true => 'success', false => 'important'}
-    styles = ['pull-right', 'badge', "badge-#{colors[player.winning_streak?]}"]
+    colors = {true => 'success', false => 'danger'}
+    styles = ['pull-right', 'label', "label-#{colors[player.winning_streak?]}"]
 
     content_tag(:span, player.winning_streak_count + player.losing_streak_count, :class => styles.join(' '))
   end
