13ff3af367646cfd724d3c03de25122e6f3445f8
James Fargher <proglottis@gmail.com>
2013-09-05 16:36:21 +1200

    Remove badge colouring that isn't in bootstrap anymore

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,7 +3,7 @@ module GamesHelper
   def pending_confirmation_badge
     count = GameRank.not_confirmed.with_participant(current_user).count
     if count > 0
-      content_tag :span, :class => 'badge badge-important' do
+      content_tag :span, :class => 'badge' do
         "#{count}"
       end
     end
