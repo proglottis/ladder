c912ec35a5e3bdfb8060530cdffb12ae5cda2ebc
Joel Walters <jtwalters@gmail.com>
2013-10-13 12:09:32 -0700

    Add contextual admin for tournaments

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -35,10 +35,11 @@ class TournamentsController < ApplicationController
     @rating_period = @tournament.current_rating_period
     @player = @tournament.players.find_by(user_id: current_user)
     @invite_request = @tournament.invite_requests.find_by(user_id: current_user)
+    @pending_invite_requests = @tournament.invite_requests.where(invite_id: nil)
     @ratings = @rating_period.ratings.with_defending_tournament.includes(:user).by_rank
     @rating_ranks = @ratings.group_by { |r| view_context.number_with_precision(r.low_rank, :precision => 0)}
     @rating = @ratings.detect { |rating| rating.user_id == current_user.id }
-    @pending = @tournament.games.where('games.confirmed_at >= ?', @rating_period.period_at)
+    @pending_games = @tournament.games.where('games.confirmed_at >= ?', @rating_period.period_at)
     @show_actions = @player.present?
   end
 
--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -7,4 +7,24 @@ module TournamentsHelper
 
     content_tag(:span, player.winning_streak_count + player.losing_streak_count, :class => styles.join(' '))
   end
+
+  def tournament_admin?
+    case
+    when
+      ['tournaments'].include?(controller_name) && params['action'] == 'edit',
+      ['invites'].include?(controller_name) && params['action'] == 'new'
+        true
+    else
+      false
+    end
+  end
+
+  def link_to_admin(tournament)
+    return unless tournament.owner_id == current_user.id
+
+    styles = []
+    styles << 'active' if tournament_admin?
+
+    content_tag(:li, link_to(t('tournaments.admin.title'), edit_tournament_path(tournament)), :class => styles.join(' '))
+  end
 end
