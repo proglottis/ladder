6f2c0149c0f260cec3ed0a1f5d18c0df13e143ca
James Fargher <proglottis@gmail.com>
2013-12-18 17:49:13 +1300

    Update whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,5 @@
 set :output, "#{path}/log/cron.log"
 
-job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
-
 every :monday, at: "00:01" do
   runner "RatingPeriodProcessor.perform"
 end
