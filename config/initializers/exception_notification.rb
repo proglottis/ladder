baecef3fdf90c0e277be184ad42d327c30ca73a7
James Fargher <proglottis@gmail.com>
2014-01-08 22:41:41 +1300

    Fix service email

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,6 +2,6 @@ if Rails.env.production?
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     :email => {
       :sender_address => 'ladders@ladders.pw',
-      :exception_recipients => ENV['LADDER_SERVICE_EMAIL']
+      :exception_recipients => ENV['SERVICE_EMAIL']
     }
 end
