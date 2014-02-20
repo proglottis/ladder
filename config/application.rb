a7edbc4a50b01ac1021b479b6bf8c480ca59dfd7
James Fargher <proglottis@gmail.com>
2014-02-20 18:33:58 +1300

    Release 1.2.11

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 module Ladder
-  VERSION = "1.2.10"
+  VERSION = "1.2.11"
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
