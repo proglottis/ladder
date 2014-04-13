4689e279339e86a85972f2a965cbd5c6f8275298
James Fargher <proglottis@gmail.com>
2014-04-13 16:00:28 +1200

    Mailer previews

---
+++ test/mailers/previews/notifications_preview.rb
@@ -0,0 +1,25 @@
+class NotificationsPreview < ActionMailer::Preview
+  def tournament_invitation
+    Notifications.tournament_invitation(Invite.last)
+  end
+
+  def game_confirmation
+    Notifications.game_confirmation(User.last, Game.last)
+  end
+
+  def game_confirmed
+    Notifications.game_confirmed(User.last, Game.last)
+  end
+
+  def challenged
+    Notifications.challenged(Game.last)
+  end
+
+  def commented
+    Notifications.commented(User.last, Comment.last)
+  end
+
+  def invite_requested
+    Notifications.invite_requested(InviteRequest.last)
+  end
+end
