64d1b919ddbc3f1b2ff2a1c63fc5ca7fbd5ab9e2
James Fargher <proglottis@gmail.com>
2014-10-09 15:21:12 +1300

    Fix Google auth issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ OmniAuth.config.test_mode = Rails.env.test?
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :google_oauth2, Rails.application.secrets.google_key, Rails.application.secrets.google_secret, {:access_type => 'online', :approval_prompt => ''}
+  provider :google_oauth2, Rails.application.secrets.google_key, Rails.application.secrets.google_secret
 end
