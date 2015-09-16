03597735178dadc52ef498c040cfa33c21e631bf
James Fargher <proglottis@gmail.com>
2015-09-17 10:09:28 +1200

    Only show tournaments the user is a participant in by default

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -3,7 +3,7 @@ module Api::V1
 class TournamentsController < ApiController
   def index
     if user_logged_in?
-      render json: Tournament.participant_or_public(current_user), each_serializer: TournamentListSerializer
+      render json: Tournament.participant(current_user), each_serializer: TournamentListSerializer
     else
       render json: Tournament.where(public: true), each_serializer: TournamentListSerializer
     end
