677dac31d7c794ba8b71e8f27446429c53b2adc5
James Fargher <proglottis@gmail.com>
2015-09-25 15:59:16 +1200

    Release v1.5.21

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Ladder
-  VERSION = "1.5.20"
+  VERSION = "1.5.21"
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
