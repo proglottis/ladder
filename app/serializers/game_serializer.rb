f4a3ee8e360e6531a4291ec63b583fd11ae513bd
James Fargher <proglottis@gmail.com>
2016-10-19 08:41:08 +1300

    Remove dead reference to serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,6 +1,6 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :created_at
-  has_one :tournament, serializer: TournamentListSerializer
+  has_one :tournament
   has_one :owner
   has_many :game_ranks
   has_many :comments
