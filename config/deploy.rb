75c517e5ab88721c9f336d0339824b646edeee79
James Fargher <proglottis@gmail.com>
2017-01-18 22:41:18 +1300

    Upstart config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,3 +7,20 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 set :keep_releases, 5
+
+namespace :sidekiq do
+  task :quiet do
+    on roles(:app) do
+      puts capture("pgrep -f 'sidekiq' | xargs kill -USR1") 
+    end
+  end
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :initctl, :restart, :worker
+    end
+  end
+end
+
+after 'deploy:starting', 'sidekiq:quiet'
+after 'deploy:reverted', 'sidekiq:restart'
+after 'deploy:published', 'sidekiq:restart'
