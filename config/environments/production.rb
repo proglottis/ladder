b4d43606c7c9702b2233cfccf7af0c459d906d79
James Fargher <proglottis@gmail.com>
2017-01-18 15:03:32 +1300

    Use SMTP for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:   Rails.application.secrets.smtp_address,
+    port:      Rails.application.secrets.smtp_port,
+    domain:    Rails.application.secrets.smtp_domain,
+    user_name: Rails.application.secrets.smtp_user_name,
+    password:  Rails.application.secrets.smtp_password
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
