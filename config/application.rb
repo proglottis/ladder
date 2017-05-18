b4a5fcfa83c58c48d800416636b7e947fadaf92f
James Fargher <proglottis@gmail.com>
2017-05-19 10:46:25 +1200

    Release v1.6.20

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Ladder
-  VERSION = "1.6.19"
+  VERSION = "1.6.20"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
