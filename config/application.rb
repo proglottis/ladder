e2d89b677fe97e521eca035bb9ea16b643c49cfc
James Fargher <proglottis@gmail.com>
2018-07-01 19:47:43 +1200

    Release v1.7.1

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Ladder
-  VERSION = "1.7.0"
+  VERSION = "1.7.1"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
